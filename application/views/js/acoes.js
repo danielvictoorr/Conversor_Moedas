97746639d2b3d4bb8956b8b523e6a68542e74eb3
daniel_victorr <danielvictor1408@gmail.com>
2023-03-30 23:52:55 -0300

    teste comentario

--- application/views/js/acoes.js
+++ application/views/js/acoes.js
@@ -1,10 +1,10 @@
-	$('#converte_moeda').on('click', function() {
-		console.log("teste");
-		// let valor= document.getElementById('val');
-		// let valor_final = valor*2;
+	// $('#converte_moeda').on('click', function() {
+	// 	console.log("teste");
+	// 	// let valor= document.getElementById('val');
+	// 	// let valor_final = valor*2;
 
-		// document.getElementById('resultado').value=valor_final;
-	})
+	// 	// document.getElementById('resultado').value=valor_final;
+	// })
 
 
 
